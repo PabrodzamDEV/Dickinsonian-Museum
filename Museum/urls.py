6bfbfa8b063d06a49df6446db4e44f4d20766383
Pablo <pabrodzamdev@gmail.com>
2024-03-24 22:24:52 +0100

    Developing front-end

--- Museum/urls.py
+++ Museum/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('poems/', views.poems, name="poems"),
-    path('novels/', views.gallery, name="gallery"),
+    path('gallery/', views.gallery, name="gallery"),
     path('essays/', views.essays, name="essays"),
     path('contact/', views.contact, name="contact"),
 ]
