fd18f4bb5ca286fbc6302ccb63ec9109969051fb
Pablo <pabrodzamdev@gmail.com>
2024-06-09 12:35:48 +0200

    Style changes

--- DickinsonDjango/settings.py
+++ DickinsonDjango/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = '/var/www/dickinsondjango/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = '/var/www/dickinsondjango/media/'
 
 customColorPalette = [
     {
