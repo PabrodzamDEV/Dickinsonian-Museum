34f9b710d2e73c4cb50f9b4f2e5b465479c49ab1
Pablo <pabrodzamdev@gmail.com>
2024-06-15 10:26:37 +0200

    Created essay detail view and download view

--- Museum/models.py
+++ Museum/models.py
@@ -10,6 +10,8 @@ from django.contrib.auth.models import User
 
 from pdf2image import convert_from_path
 
+from DickinsonDjango.settings import env
+
 
 class Poem(models.Model):
     CATEGORY_CHOICES = [
